dec954477337f748d7ce981a0230e5f765239690
Athisruth M Satheesh <athisruth@outlook.com>
2026-04-30 01:07:17 +0530

    "useeffect added"

--- anatomy_lab/src/App.tsx
+++ anatomy_lab/src/App.tsx
@@ -14,6 +14,13 @@ function App() {
 
   useEffect(() => {
     fetchSession();
+    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    }
+    );
+    return () => {
+      authListener.subscription.unsubscribe();
+    }
   }, []);
 
   const logout = async () => {
