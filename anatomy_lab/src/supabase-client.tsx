12a17a299534ce1932ffa9ced63c91609c3fda67
Athisruth M Satheesh <athisruth@outlook.com>
2026-04-29 16:28:27 +0530

    added

---
+++ anatomy_lab/src/supabase-client.tsx
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+
+
+
+export const supabase = createClient("https://livqnevngeavqcvafxex.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpdnFuZXZuZ2VhdnFjdmFmeGV4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NzQ0NDI1NCwiZXhwIjoyMDkzMDIwMjU0fQ.W57K62q33Oatv-OfCF4bSIIFIJkOYQg8JjRJ4EC7Pt4",
+
+);
